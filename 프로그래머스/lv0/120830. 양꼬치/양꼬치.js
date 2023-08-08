405534c1a20b1a60110446a114423fb834a01bc9
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2023-08-08 22:43:54 +0900

    [level 0] Title: 양꼬치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,7 +1,3 @@
 function solution(n, k) {
-    if (parseInt(n/10) !==0){
-       return 12000*n + 2000*k - 2000*parseInt(n/10); 
-    }  else{
-        return 12000*n + 2000*k;
-    }
+       return 12000*n + 2000*k - 2000*parseInt(n/10);
 }
