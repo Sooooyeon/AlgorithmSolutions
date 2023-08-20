4e2a8eda0a82ceb87906f7243746c595f60c2108
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2023-08-20 21:11:16 +0900

    [level 0] Title: 가까운 수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -0,0 +1,8 @@
+function solution(array, n) {
+    return array[
+        array
+            .sort((a, b) => a - b)
+            .map((v) => Math.abs(v - n))
+            .indexOf(Math.min(...array.map((v) => Math.abs(v - n))))
+    ];
+}
