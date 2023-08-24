3f13bd9563e483403039bbd42ca4c73e086d0d37
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2023-08-25 03:07:43 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,6 @@
+function solution(numbers) {
+    const num = {
+        zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9
+    }
+    return +numbers.match(/one|two|three|four|five|six|seven|eight|nine|zero/g).map(a => num[a]).join("")
+}
