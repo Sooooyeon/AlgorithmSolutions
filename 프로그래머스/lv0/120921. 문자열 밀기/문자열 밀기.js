0b8fb34c3b5a026d873d88bd40a0c4ba85f16d17
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2023-08-26 15:46:59 +0900

    [level 0] Title: 문자열 밀기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,10 +1,4 @@
 function solution(A, B) {
-    let count = 0
-    A = A.split("")
-    while(count < A.length) {
-        if(A.join("") === B) return count
-        A.unshift(A.pop())
-        count++
-    }
-    return -1
+    var answer = 0;
+    return answer;
 }
