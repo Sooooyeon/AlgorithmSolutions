b95a517ca179a1f118ff44c7a77e1910806cd4c1
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2023-08-27 15:16:22 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,9 +1,4 @@
 function solution(s) {
-    const arr = [];
-    s.split(" ").map((v) => {
-    if (v !== 'Z') { arr.push(Number(v)); }
-    else { arr.pop() }
-    return arr;
-});
-    return arr.reduce((a, c) => a + c, 0);
+    var answer = 0;
+    return answer;
 }
