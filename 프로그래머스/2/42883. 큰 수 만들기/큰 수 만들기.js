6b2d95167de44ef510d2d5039ca4ae7afc7db36d
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2023-12-07 00:56:26 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -0,0 +1,14 @@
+function solution(number, k) {
+  let answer = [];
+
+  for (let i = 0; i < number.length; i++) {
+    while (k > 0 && answer[answer.length - 1] < number[i]) {
+      answer.pop();
+      k--;
+    }
+    answer.push(number[i]);
+  }
+  answer = answer.join("").slice(0, number.length - k);
+  return answer;
+}
+
