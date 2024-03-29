080fa56ca7190c9658a19ae3f1a0932c0403651a
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2024-03-29 23:28:27 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.18 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/README.md
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 홀짝에 따라 다른 값 반환하기 - 181935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181935?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181935) 
 
 ### 성능 요약
 
-메모리: 31.2 MB, 시간: 0.19 ms
+메모리: 31.2 MB, 시간: 0.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 29일 23:21:09
+2024년 03월 29일 23:28:25
 
 ### 문제 설명
 
--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.cs
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.cs
@@ -4,17 +4,13 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
         if(n % 2 == 0){
-            for(int i = 1; i < n+1; i++){
-                if(i % 2 == 0){
+            for(int i = 2; i < n+1; i+=2){
                     answer += i * i;
-                }
             }
         }
         if(n % 2 == 1){
-            for(int i = 1; i < n+1; i++){
-                if(i % 2 == 1){
-                    answer += i;
-                }
+            for(int i = 1; i < n+1; i+=2){
+                answer += i;
             }
         }
         return answer;
