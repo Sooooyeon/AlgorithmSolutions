e2dc8cd36321f1e64447dd9238978ce07fbaf2e0
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2024-04-28 21:37:18 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -2,7 +2,7 @@ function solution(s) {
     let answer = s;
     let arrNum = ["zero","one","two","three","four","five","six","seven","eight","nine"]
     for(let i = 0; i<10; i++){
-        answer = answer.replaceAll(arrNum[i],i)
+        answer = answer.replace(arrNum[i],i)
     }
     return parseInt(answer);
 }
