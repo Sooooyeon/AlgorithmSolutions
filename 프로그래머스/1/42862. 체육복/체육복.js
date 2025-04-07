101dcb03a515787190debb28a672a4446c18b7a6
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2025-04-07 22:04:20 +0900

    [level 1] Title: 체육복, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -18,3 +18,29 @@ function solution(n, lost, reserve){
   const answer = count.filter(v=> v>=0).length;
   return answer;
 }
+
+/* */
+
+/*
+
+
+
+function solution(n, lost, reserve) {
+    
+    lost.sort((a, b) => a - b);
+    reserve.sort((a, b) => a - b);
+
+    lost = lost.filter(v => !reserve.includes(v));
+    reserve = reserve.filter(v => !lost.includes(v));
+
+    reserve.forEach(v => {
+        if (lost.includes(v - 1)) {
+            lost = lost.filter(i => i !== v - 1); // 왼쪽 학생에게 빌려줌
+        } else if (lost.includes(v + 1)) {
+            lost = lost.filter(i => i !== v + 1); // 오른쪽 학생에게 빌려줌
+        }
+    });
+
+    return n - lost.length;
+}
+*/
